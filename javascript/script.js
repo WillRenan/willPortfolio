0f1958c119ce84704ae7806725b84f6e969b9557
WillRenan <willdesign,will@gmail.com>
2021-06-18 14:10:12 -0300

    Adicionando primeiros elements

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+var nome = 'wilian';
+var text = ` O nome deses indivíduo é ${nome}`; // templates string
+console.log(text);
+
+
+{//scopo fechado sem preciasr de funções 
+    let apelido = 'willian';
+
+}
+console.log (apelido);
